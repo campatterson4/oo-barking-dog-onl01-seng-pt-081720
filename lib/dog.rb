40a77448ff1ec2dace6d8feff10e2cf3e16a2fef
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-02 02:42:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,15 @@
 class Dog
   def name=(dogs_name)
     @name = dogs_name
-  end 
+end 
 
   def name
     @name
-  end 
-  
+end 
 end 
 
 class Dog
   def bark
     puts "woof!"
-  end 
-  
+end 
 end 
